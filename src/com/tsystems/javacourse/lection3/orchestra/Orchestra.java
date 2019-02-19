6880539951d32e9cb1c7f3cfdbfa75240e6b26db
Danil Katashev <47444302+dkatashe@users.noreply.github.com>
2019-02-19 07:57:18 +0300

    Lection 3 orchestra - remove switch

--- src/com/tsystems/javacourse/lection3/orchestra/Orchestra.java
+++ src/com/tsystems/javacourse/lection3/orchestra/Orchestra.java
@@ -7,24 +7,7 @@ public class Orchestra
 {
   private ArrayList<Instrument> instruments = new ArrayList<>();
 
-  private void addInstrument(String name) {
-    Instrument i = null;
-
-    switch (name) {
-      case "Violin":
-        i = new Violin();
-        break;
-      case "Flute":
-        i = new Flute();
-        break;
-      case "Oboe":
-        i = new Oboe();
-        break;
-      case "Cello":
-        i = new Cello();
-        break;
-    }
-
+  private void addInstrument(Instrument i) {
     instruments.add(i);
   }
 
@@ -34,11 +17,11 @@ public class Orchestra
 
   public static void main(String[] args)
   {
-    Orchestra orchestra  = new Orchestra();
-    orchestra.addInstrument("Violin");
-    orchestra.addInstrument("Flute");
-    orchestra.addInstrument("Oboe");
-    orchestra.addInstrument("Cello");
+    Orchestra orchestra = new Orchestra();
+    orchestra.addInstrument(new Violin());
+    orchestra.addInstrument(new Flute());
+    orchestra.addInstrument(new Oboe());
+    orchestra.addInstrument(new Cello());
     orchestra.playConcert();
   }
 }
